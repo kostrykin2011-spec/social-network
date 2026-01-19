68afad33d563b2a516cee3e9159b4c968768fd03
AlexKostrykin <kostrykin2011@gmail.com>
2026-01-19 21:05:38 +0300

    Убран код для наполонения кеша c uuid-пользователей

--- chat-service/cmd/main.go
+++ chat-service/cmd/main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"chat-service/config"
 	"chat-service/internal/handlers"
-	"chat-service/internal/helpers"
 	"chat-service/internal/repository"
 	"chat-service/internal/services"
 	"chat-service/pkg/database"
-	"encoding/csv"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -29,30 +24,6 @@ func main() {
 	messageRepository := repository.InitMessageRepository(db)
 	dialogRepository := repository.InitDialogRepository(db)
 
-	filePath := "/app/users.csv"
-	if filePath == "" {
-		fmt.Println("Файл people.csv не найден")
-	}
-
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Ошибка при открытии файла:", err)
-	}
-
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Ошибка при чтении CSV:", err)
-	}
-
-	globalUUIDCache := helpers.Instance()
-	for _, record := range records {
-		userId, _ := uuid.Parse(record[0])
-		globalUUIDCache.Add(userId)
-	}
-	
 	messageService := services.InitMessageService(dialogRepository, messageRepository)
 
 	routes := handlers.InitRoutes(messageService)
@@ -66,5 +37,30 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Не удалось запустить сервер: %v", err)
 	}
+}
 
+func loadTestData() {
+	// filePath := "/app/users.csv"
+	// if filePath == "" {
+	// 	fmt.Println("Файл people.csv не найден")
+	// }
+
+	// file, err := os.Open(filePath)
+	// if err != nil {
+	// 	fmt.Println("Ошибка при открытии файла:", err)
+	// }
+
+	// defer file.Close()
+
+	// reader := csv.NewReader(file)
+	// records, err := reader.ReadAll()
+	// if err != nil {
+	// 	fmt.Println("Ошибка при чтении CSV:", err)
+	// }
+
+	// globalUUIDCache := helpers.Instance()
+	// for _, record := range records {
+	// 	userId, _ := uuid.Parse(record[0])
+	// 	globalUUIDCache.Add(userId)
+	// }
 }
